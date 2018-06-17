4079697cdb501910945fff2a1c313fd24fa4a6ac
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2018-06-17 15:26:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
 });
 
 const prefix = "h!";
